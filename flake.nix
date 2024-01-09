56364db314f946e76e73b43312046debe936e40a
Steven Rhodes <rhodes.steven@gmail.com>
2024-01-09 11:13:03 -0800

    Add flake and vendor

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "arris-scrape will read from Arris's modem web page and output Prometheus-style
+metrics as a one-shot.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.buildGoModule {
+          name = "arris-scrape";
+          src = ./.;
+          vendorHash = null;
+        };
+      });
+    };
+}
